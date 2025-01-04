8c9c568e70118955cc777eea1a6bdd0ba117ddf0
stkisengese <skisengese@outlook.com>
2025-01-04 18:39:12 +0300

    feat: add REPL feature

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println(`Hello, World!`)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+
+		words := cleanInput(input)
+		if len(words) > 0 {
+			fmt.Printf("Your command was: %s\n", words[0])
+		}
+	}
 }
 
 func cleanInput(text string) []string {
