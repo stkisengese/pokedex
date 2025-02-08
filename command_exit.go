7fc338e8b30f873c281af14ac925963e942ec978
skisenge <skisengese@gmail.com>
2025-02-08 15:38:43 +0300

    fix: apply cache

--- command_exit.go
+++ command_exit.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/stkisengese/pokedex/internal/models"
 )
 
-func commandExit(cfg *config) error {
+func commandExit(cfg *models.Config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
