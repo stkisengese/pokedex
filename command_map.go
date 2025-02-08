887a5b7c1e9b542c7ec07b1a917c235f385473f3
skisenge <skisengese@gmail.com>
2025-02-08 06:10:46 +0300

    refactor: add handleMapRequest function

--- command_map.go
+++ command_map.go
@@ -12,7 +12,23 @@ func commandMap(cfg *config) error {
 	if url == "" {
 		url = "https://pokeapi.co/api/v2/location-area?limit=20"
 	}
+	return handelMapRequest(url, cfg)
+}
 
+// commandMapBack handles the 'mapb' command
+func commandMapBack(cfg *config) error {
+	if cfg.PreviousURL == "" {
+		fmt.Println("you're on the first page")
+		return nil
+	}
+	return handelMapRequest(cfg.PreviousURL, cfg)
+}
+
+// handelMapRequest sends an HTTP GET request to the specified URL and processes
+// the response. It decodes the JSON response into a pokeAPIResponse struct,
+// prints the names of areas from the response, and updates the configuration
+// with the next and previous URLs if available.
+func handelMapRequest(url string, cfg *config) error {
 	resp, err := http.Get(url)
 	if err != nil {
 		return fmt.Errorf("error fetching data: %v", err)
@@ -23,43 +39,6 @@ func commandMap(cfg *config) error {
 	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
 		return fmt.Errorf("error decoding response: %v", err)
 	}
-
-	for _, area := range response.Results {
-		fmt.Println(area.Name)
-	}
-
-	if response.Next != nil {
-		cfg.NextURL = *response.Next
-	} else {
-		cfg.NextURL = ""
-	}
-
-	if response.Previous != nil {
-		cfg.PreviousURL = *response.Previous
-	} else {
-		cfg.PreviousURL = ""
-	}
-	return nil
-}
-
-// commandMapBack handles the 'mapb' command
-func commandMapBack(cfg *config) error {
-	if cfg.PreviousURL == "" {
-		fmt.Println("you're on the first page")
-		return nil
-	}
-
-	resp, err := http.Get(cfg.PreviousURL)
-	if err != nil {
-		return fmt.Errorf("error fetching data: %v", err)
-	}
-	defer resp.Body.Close()
-
-	var response pokeAPIResponse
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return fmt.Errorf("error decoding response: %v", err)
-	}
-
 	for _, area := range response.Results {
 		fmt.Println(area.Name)
 	}
