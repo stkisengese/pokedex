c361058f203d5047ffa9ae98ff3f968c7032ad02
skisenge <skisengese@gmail.com>
2025-02-08 16:36:54 +0300

    fix: fix number of parameters

--- command_exit.go
+++ command_exit.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stkisengese/pokedex/internal/models"
 )
 
-func commandExit(cfg *models.Config) error {
+func commandExit(cfg *models.Config, args ...string) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
