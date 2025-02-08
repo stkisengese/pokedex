a1dccb4bd89d335f39aca6150acf375bad3538b9
skisenge <skisengese@gmail.com>
2025-02-08 05:07:44 +0300

    fix: fix help command output

--- command_help.go
+++ command_help.go
@@ -2,16 +2,14 @@ package main
 
 import "fmt"
 
-func commandHelp(commands map[string]cliCommand) func(*config) error {
-	return func(*config) error {
-		fmt.Println()
-		fmt.Println("\nWelcome to the Pokedex!")
-		fmt.Println("Usage:")
-		fmt.Println()
-		for _, cmd := range commands {
-			fmt.Printf("%s: %s\n", cmd.name, cmd.description)
-		}
-		fmt.Println()
-		return nil
+func commandHelp(cfg *config) error {
+	fmt.Println()
+	fmt.Println("\nWelcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
+	fmt.Println()
+	return nil
 }
