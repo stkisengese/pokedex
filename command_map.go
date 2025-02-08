a829d3b63e5b6ec1d734849b4bb503a37f889020
skisenge <skisengese@gmail.com>
2025-02-08 15:40:01 +0300

    chore: refactor map request

--- command_map.go
+++ command_map.go
@@ -1,56 +1,26 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
+
+	"github.com/stkisengese/pokedex/internal/models"
+	"github.com/stkisengese/pokedex/internal/pokeapi"
 )
 
 // commandMap handles the 'map' command
-func commandMap(cfg *config) error {
+func commandMap(cfg *models.Config) error {
 	url := cfg.NextURL
 	if url == "" {
 		url = "https://pokeapi.co/api/v2/location-area?limit=20"
 	}
-	return handelMapRequest(url, cfg)
+	return pokeapi.HandelMapRequest(url, cfg)
 }
 
 // commandMapBack handles the 'mapb' command
-func commandMapBack(cfg *config) error {
+func commandMapBack(cfg *models.Config) error {
 	if cfg.PreviousURL == "" {
 		fmt.Println("you're on the first page")
 		return nil
 	}
-	return handelMapRequest(cfg.PreviousURL, cfg)
-}
-
-// handelMapRequest sends an HTTP GET request to the specified URL and processes
-// the response. It decodes the JSON response into a pokeAPIResponse struct,
-// prints the names of areas from the response, and updates the configuration
-// with the next and previous URLs if available.
-func handelMapRequest(url string, cfg *config) error {
-	resp, err := http.Get(url)
-	if err != nil {
-		return fmt.Errorf("error fetching data: %v", err)
-	}
-	defer resp.Body.Close()
-
-	var response pokeAPIResponse
-	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
-		return fmt.Errorf("error decoding response: %v", err)
-	}
-	for _, area := range response.Results {
-		fmt.Println(area.Name)
-	}
-	if response.Next != nil {
-		cfg.NextURL = *response.Next
-	} else {
-		cfg.NextURL = ""
-	}
-	if response.Previous != nil {
-		cfg.PreviousURL = *response.Previous
-	} else {
-		cfg.PreviousURL = ""
-	}
-	return nil
+	return pokeapi.HandelMapRequest(cfg.PreviousURL, cfg)
 }
